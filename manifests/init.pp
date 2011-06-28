25944b8072f0cfa0f5bd27b32dbe58e784ea8edf
Cody Herriges <c.a.herriges@gmail.com>
2011-06-28 16:16:40 -0700

    Initial skeleton commit.

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: mrepo
+#
+# This module manages mrepo
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class mrepo {
+
+
+}
