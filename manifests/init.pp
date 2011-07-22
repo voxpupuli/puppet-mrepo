56abd9282ef3b77fd40ebc2ad65bf09245815712
Adrien Thebo <adrien@puppetlabs.com>
2011-07-22 11:17:56 -0700

    Moved mrepo package resources to mrepo::package

--- manifests/init.pp
+++ manifests/init.pp
@@ -22,8 +22,6 @@
 # outside of puppet and only want dependencies to be installed, then include
 # this class.
 class mrepo {
-
-  include mrepo::params
   include mrepo::package
   include mrepo::rhn
   include mrepo::webservice
@@ -31,42 +29,7 @@ class mrepo {
 
   Class['mrepo::package']    -> Class['mrepo::webservice']
   Class['mrepo::package']    -> Class['mrepo::rhn']
-  Class['mrepo']             -> Mrepo::Repo <| |>
-  Class['mrepo']             -> Class['mrepo::selinux']
+  Class['mrepo::package']    -> Class['mrepo::selinux']
+  Class['mrepo::webservice'] -> Class['mrepo::selinux']
 
-  $user = $mrepo::params::user
-  $group = $mrepo::params::group
-
-  file { "/etc/mrepo.conf":
-      ensure  => present,
-      owner   => $user,
-      group   => $group,
-      mode    => '0640',
-      content => template("mrepo/mrepo.conf.erb"),
-  }
-
-  file {
-    "/etc/mrepo.conf.d":
-      ensure  => directory,
-      owner   => $user,
-      group   => $group,
-      mode    => '0755';
-    "/var/cache/mrepo":
-      ensure  => directory,
-      owner   => $user,
-      group   => $group,
-      mode    => '0755';
-    $mrepo::params::src_root:
-      ensure  => directory,
-      owner   => $user,
-      group   => $group,
-      mode    => '0755';
-  }
-
-  package {
-    "lftp":
-      ensure  => present;
-    "createrepo":
-      ensure  => present;
-  }
 }
--- manifests/package.pp
+++ manifests/package.pp
@@ -9,7 +9,7 @@
 #   Installs mrepo in the manner specified by mrepo::params
 #
 # Requires:
-#   - puppetlabs-vcsrepo
+#   - puppetlabs-vcsrepo for puppet < 2.7.2
 #
 # Sample Usage:
 #   This class does not need to be directly included
@@ -17,12 +17,15 @@ class mrepo::package {
 
   include mrepo::params
 
+  $user = $mrepo::params::user
+  $group = $mrepo::params::group
   $source = $mrepo::params::source
 
   case $source {
     git: {
       vcsrepo { "/usr/src/mrepo":
         ensure    => latest,
+        revision  => 'HEAD',
         provider  => "git",
         source    => "git://github.com/dagwieers/mrepo.git",
       }
@@ -41,5 +44,39 @@ class mrepo::package {
         ensure  => present,
       }
     }
+  }
+
+  file { "/etc/mrepo.conf":
+    ensure  => present,
+    owner   => $user,
+    group   => $group,
+    mode    => '0640',
+    content => template("mrepo/mrepo.conf.erb"),
+  }
+
+  file {
+    "/etc/mrepo.conf.d":
+      ensure  => directory,
+      owner   => $user,
+      group   => $group,
+      mode    => '0755';
+    "/var/cache/mrepo":
+      ensure  => directory,
+      owner   => $user,
+      group   => $group,
+      mode    => '0755';
+    $mrepo::params::src_root:
+      ensure  => directory,
+      owner   => $user,
+      group   => $group,
+      mode    => '0755';
+  }
+
+  # Packages needed to mirror files and generate mirror metadata
+  package {
+    "lftp":
+      ensure  => present;
+    "createrepo":
+      ensure  => present;
   }
 }
