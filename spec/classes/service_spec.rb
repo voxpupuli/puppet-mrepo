4c51c6e22a99992af2522ea6bfeb252aa13bc311
Pascal  BOURDIER <p.bourdier@meetic-corp.com>
2017-01-26 14:12:44 +0100

    follow bbatsov/ruby-style-guide

--- spec/classes/service_spec.rb
+++ spec/classes/service_spec.rb
@@ -12,20 +12,24 @@ describe 'mrepo::service', type: :class do
       end
 
       context 'with service mrepo manage and disable' do
-        let(:params) { {
-          service_enable: false,
-          service_manage: true,
-        } }
+        let(:params) do
+          {
+            service_enable: false,
+            service_manage: true
+          }
+        end
 
         it { is_expected.to compile.with_all_deps }
         it { is_expected.to contain_service('mrepo').with('enable' => false) }
       end
 
       context 'with service mrepo manage and enable' do
-        let(:params) { {
-          service_enable: true,
-          service_manage: true,
-        } }
+        let(:params) do
+          {
+            service_enable: true,
+            service_manage: true
+          }
+        end
 
         it { is_expected.to compile.with_all_deps }
         it { is_expected.to contain_service('mrepo').with('enable' => true) }
